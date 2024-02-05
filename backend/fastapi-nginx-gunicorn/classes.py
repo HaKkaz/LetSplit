940a199cbade99c529c0721b0569b1a94ba87801
HaKkaz <luckyanthonyan@gmail.com>
2024-02-05 16:12:10 +0800

    Add event create time.

--- backend/fastapi-nginx-gunicorn/classes.py
+++ backend/fastapi-nginx-gunicorn/classes.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from utils import getDatetime
+from datetime import datetime
 
 class Item(BaseModel):
     datetime: str = getDatetime()
@@ -14,12 +15,14 @@ class User(BaseModel):
 
 class Event(BaseModel):
     event_name: str
+    event_time: datetime = getDatetime()
     user_list: list[User]
     item_list: list[Item] = list()
 
     def to_dict(self) -> dict:
         result = {
             "event_name": self.event_name,
+            "event_time": self.event_time,
             "user_list": [user.__dict__ for user in self.user_list],
             "item_list": [item.__dict__ for item in self.item_list],
         }
